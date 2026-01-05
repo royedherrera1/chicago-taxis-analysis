e676d1941e9fcc403ba776e19fc0a070d763c956
royedherrera1 <royedherrera1@gmail.com>
2026-01-05 16:17:45 -0600

    Update 03_monthly_aggregations.sql

--- sql/03_monthly_aggregations.sql
+++ sql/03_monthly_aggregations.sql
@@ -1,1 +1,15 @@
+-- Monthly aggregated metrics used for Tableau dashboard
+
+SELECT
+  DATE_TRUNC(DATE(trip_start_timestamp), MONTH) AS mes,
+  COUNT(*) AS total_viajes,
+  SUM(trip_total) AS ingresos_totales,
+  AVG(trip_total) AS ticket_promedio,
+  AVG(trip_seconds) / 60 AS duracion_promedio_min
+FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+WHERE trip_start_timestamp IS NOT NULL
+  AND trip_total IS NOT NULL
+  AND trip_miles > 0
+GROUP BY mes
+ORDER BY mes;
 
