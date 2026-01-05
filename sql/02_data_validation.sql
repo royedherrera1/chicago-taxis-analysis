c238fee54f5a98f72a26ca957226e6536a961bda
royedherrera1 <royedherrera1@gmail.com>
2026-01-05 16:24:03 -0600

    Update 02_data_validation.sql

--- sql/02_data_validation.sql
+++ sql/02_data_validation.sql
@@ -1,3 +1,19 @@
+-- NULL profiling for key analytical fields
+SELECT
+  COUNT(*) AS total_rows,
+  COUNT(trip_start_timestamp) AS trip_start_no_null,
+  COUNT(trip_end_timestamp) AS trip_end_no_null,
+  COUNT(trip_seconds) AS trip_seconds_no_null,
+  COUNT(trip_miles) AS trip_miles_no_null,
+  COUNT(fare) AS fare_no_null,
+  COUNT(tips) AS tips_no_null,
+  COUNT(trip_total) AS trip_total_no_null,
+  COUNT(payment_type) AS payment_type_no_null,
+  COUNT(company) AS company_no_null
+FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`;
+
+-- Validate trip_total against fare components
+
 SELECT
   trip_total,
   fare,
