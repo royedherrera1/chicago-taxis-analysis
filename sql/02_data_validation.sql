ef58e87f185ad232e2858faf24cc01e90d2b447c
royedherrera1 <royedherrera1@gmail.com>
2026-01-05 16:19:11 -0600

    Update 02_data_validation.sql

--- sql/02_data_validation.sql
+++ sql/02_data_validation.sql
@@ -4,8 +4,16 @@ SELECT
   tips,
   tolls,
   extras,
-  COALESCE(fare, 0) + COALESCE(tips, 0) + COALESCE(tolls, 0) + COALESCE(extras, 0) AS total_calculado,
-  ABS(trip_total- (COALESCE(fare, 0) + COALESCE(tips, 0) + COALESCE(tolls, 0) + COALESCE(extras, 0))) AS diferencia
+  COALESCE(fare, 0) 
+  + COALESCE(tips, 0) 
+  + COALESCE(tolls, 0) 
+  + COALESCE(extras, 0) AS total_calculado,
+  ABS(
+  trip_total- 
+  (COALESCE(fare, 0) 
+  + COALESCE(tips, 0) 
+  + COALESCE(tolls, 0) 
+  + COALESCE(extras, 0))) AS diferencia
 FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
 WHERE trip_total IS NOT NULL
   AND trip_miles > 0
