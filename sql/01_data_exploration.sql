b2b2ff1a7fd060c45ae9f46060f0f71240bc7312
royedherrera1 <royedherrera1@gmail.com>
2026-01-05 16:21:52 -0600

    Update 01_data_exploration.sql

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -1,1 +1,10 @@
+-- Dataset overview
+SELECT COUNT(*) AS total_trips
+FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`;
+
+-- Date range
+SELECT
+  MIN(trip_start_timestamp) AS min_date,
+  MAX(trip_start_timestamp) AS max_date
+FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`;
 
